0621a0937921fb5fa0ac75e835a4b22fa6db471a
Prakrati <prakrati0307@gmail.com>
2019-04-24 11:08:58 -0400

    Initial commit

---
+++ app/src/main/java/prakratisharma/nycschools/mvvm_fragmentrecyler/MainActivity.java
@@ -0,0 +1,13 @@
+package prakratisharma.nycschools.mvvm_fragmentrecyler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
